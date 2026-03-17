b0d7127b9f0fbb9337283edcfd5ce09ed2f07c6b
Cau0x01 <pixelc016@gmail.com>
2026-03-17 16:53:54 -0300

    db

---
+++ models.py
@@ -0,0 +1,65 @@
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
+from sqlalchemy.orm import declarative_base
+from sqlalchemy_utils.types import ChoiceType
+
+# criar a conexão do seu banco
+db = create_engine("sqlite:///banco.db")
+
+# criar a base do banco de de dados
+Base = declarative_base()
+
+#criar as classes/tabelas do banco
+
+class User(Base):
+    __tablename__="usuarios"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    name = Column("name", String)
+    email = Column("email", String, nullable=False)
+    pin = Column("pin",Integer)
+    ativo = Column("ativo",Boolean)
+    admin = Column("admin",Boolean, default=False)
+
+    def __init__(self, name, email, pin, ativo=True, admin=False):
+        self.name = name
+        self.email = email
+        self.pin = pin
+        self.ativo = ativo
+        self.admin = admin
+
+# padidos
+
+class pedido(Base):
+    __tablename__="pedidos"
+
+    STATUS_PEDIDOS = (
+        ("PENDENTE", "PENDENTE")
+        ("CANCELADO", "CANCELADO")
+        ("FINALIZADO", "FINALIZADO")
+    )
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    status = Column("status", ChoiceType(choices=STATUS_PEDIDOS)) #pendente, cancelado, finalizado
+    user = Column("user", ForeignKey("usuarios.id"))
+    price = Column("price", Float,)
+    # itens = Column("itens",)
+
+    def __init__(self, user, status="PENDENTE", price=0):
+        self.user = user
+        self.price = price
+        self.status = status
+
+#itenspedido
+
+class ItemPedido(Base):
+    __tablename__="itens_pedido"
+
+    id = Column("id", Integer, primary_key=True, autoincrement=True)
+    amount = Column("amount", Integer)
+    flavor = Column("flavor", String)
+    size = Column("size", String)
+    price_unit = Column("price_unit", Float)
+    pedido = Column("pedido", ForeignKey("pedidos.id"))
+
+
+# executa a criação dos metadados do seu banco (criar efetivamente o banco de dados)C
--- order_routes.py
+++ order_routes.py
@@ -6,5 +6,5 @@ order_router = APIRouter(prefix="/request", tags=["request"])
 async  def request():
     """
     Essa é a rota padrão de pedidos do nosso sistema. Todas as rotas dos pedidos precisam de autenticação
-    """
+    """ 
     return {"mensage": "Você acessou a rota de pedidos"}
