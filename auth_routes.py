b9afc806db3618f32108c84e80997e12dfa7b103
Cau0x01 <pixelc016@gmail.com>
2026-03-30 17:00:55 -0300

    att login & tokens

--- auth_routes.py
+++ auth_routes.py
@@ -1,20 +1,25 @@
 from fastapi import APIRouter, Depends, HTTPException
 from models import User
 from dependencies import get_session
-from main import bcrypt_context
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINITES, SECRET_KEY
 from schemas import UserSchema, LoginSchema
 from sqlalchemy.orm import Session
-
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-def create_token(id_user):
-    # JWT
-    # id_user
-    #datd=a_expiracao
-    
-    token = f"75SrSS0l0tqdHyCXgUqSpIDTuicAAFfP2OC7dWoOx3Kj17UrIJUOlrpJYfZWw8k4{id_user}"
-    return token
+def create_token(id_user, duration_token = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINITES)):
+    expiration_date = datetime.now(timezone.utc) + duration_token
+    dic_info = {"sub": id_user, "exp": expiration_date}
+    jwt_encoded = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+    return jwt_encoded
+
+def verify_token(token, session: Session = Depends(get_session)):
+    # verificar se o token é valido
+    # extrair o ID do usuario do token
+    user = session.query(User).filter(User.id==1).first()
+    return user
 
 def autenty_user(email, password, session):
     user = session.query(User).filter(User.email==email).first()
@@ -53,8 +58,16 @@ async def login(login_schema: LoginSchema, session: Session = Depends(get_sessio
         raise HTTPException(status_code=400, detail="Usuário não encontrado ou credenciais invalidas")
     else:
         access_token = create_token(user.id)
+        refresh_token = create_token(user.id, duration_token=timedelta(days=7))
         return {"access_token": access_token,
+                "refresh_token": refresh_token,
                 "token_type": "Bearer"
                 }
 #JWT Bearer 
 # headers = {"Access-Token": "dearer token"}
+
+@auth_router.get("/refresh")
+async def user_refresh_token(token):
+    #verificar o token
+    user = verify_token(token)
+    access_token = create_token(user.id)
